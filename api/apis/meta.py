7062cc171fe85378a439c730d3960eabee6695ae
icesun <saisun.ss@gmail.com>
2015-09-11 10:38:31 +1000

    Change Course.py

--- api/apis/meta.py
+++ api/apis/meta.py
@@ -64,7 +64,7 @@ def meta_courseinfo(request):
         coursedb = api.views.get_course(course['id'])
 
         filename = uqx_api.courses.EDX_DATABASES[db]['discussiontable'].replace("/", "-").replace("-prod", "")
-        courseurl = config.SERVER_URL + '/datasources/course_structure/'+filename+'.json';
+        courseurl = config.SERVER_URL + '/datasources/course_structure/'+filename+'.json'
         data = '[]'
         try:
             data = urllib2.urlopen(courseurl).read().replace('<script','').replace('</script>','')
