a4c00978da60a3659b1236e7878504318a0f30d4
icesun <saisun.ss@gmail.com>
2016-11-11 11:58:30 +1000

    update courses.py

--- uqx_api/courses.py
+++ uqx_api/courses.py
@@ -17,6 +17,7 @@ EDX_DATABASES = {
     'crime_101x_3T2014': {'dbname': 'UQx_Crime101x_3T2014', 'mongoname': 'UQx/Crime101x/3T2014', 'discussiontable': 'UQx-Crime101x-3T2014-prod', 'csjson': 'UQx-Crime101x-3T2014-course_structure-prod-analytics.json', 'icon': 'fa-gavel', 'year': '2014', 'term': '3T'},
     'crime_101x_2T2015': {'dbname': 'UQx_Crime101x_2T2015', 'mongoname': 'UQx/Crime101x/2T2015', 'discussiontable': 'UQx-Crime101x-2T2015-prod', 'csjson': 'UQx-Crime101x-2T2015-course_structure-prod-analytics.json', 'icon': 'fa-gavel', 'year': '2015', 'term': '2T'},
     'crime_101x_1T2016': {'dbname': 'UQx_Crime101x_1T2016', 'mongoname': 'course-v1:UQx+Crime101x+1T2016', 'discussiontable': 'UQx-Crime101x-1T2016-prod', 'csjson': 'UQx-Crime101x-1T2016-course_structure-prod-analytics.json', 'icon': 'fa-gavel', 'year': '2016', 'term': '1T'},
+	'crime_101x_3T2016': {'dbname': 'UQx_Crime101x_3T2016', 'mongoname': 'course-v1:UQx+Crime101x+3T2016', 'discussiontable': 'UQx-Crime101x-3T2016-prod', 'csjson': 'UQx-Crime101x-3T2016-course_structure-prod-analytics.json', 'icon': 'fa-gavel', 'year': '2016', 'term': '3T'},
 
     'denial_101x_1T2015': {'dbname': 'UQx_Denial101x_1T2015', 'mongoname': 'UQx/Denial101x/1T2015', 'discussiontable': 'UQx-Denial101x-1T2015-prod', 'csjson': 'UQx-Denial101x-1T2015-course_structure-prod-analytics.json', 'icon': 'fa-recycle', 'year': '2015', 'term': '1T'},
     'denial_101x_2T2015': {'dbname': 'UQx_Denial101x_2T2015', 'mongoname': 'course-v1:UQx+Denial101x+2T2015', 'discussiontable': 'UQx-Denial101x-2T2015-prod', 'csjson': 'UQx-Denial101x-2T2015-course_structure-prod-analytics.json', 'icon': 'fa-recycle', 'year': '2015', 'term': '2T'},
@@ -26,8 +27,18 @@ EDX_DATABASES = {
     'hypers_301x_1T2014': {'dbname': 'UQx_HYPERS301x_1T2014', 'mongoname': 'UQx/HYPERS301x/1T2014', 'discussiontable': 'UQx-HYPERS301x-1T2014-prod', 'csjson': 'UQx-HYPERS301x-1T2014-course_structure-prod-analytics.json', 'icon': 'fa-plane', 'year': '2014', 'term': '1T'},
     'hypers_301x_1T2015': {'dbname': 'UQx_HYPERS301x_1T2015', 'mongoname': 'UQx/HYPERS301x/1T2015', 'discussiontable': 'UQx-HYPERS301x-1T2015-prod', 'csjson': 'UQx-HYPERS301.x-1T2015-course_structure-prod-analytics.json', 'icon': 'fa-plane', 'year': '2015', 'term': '1T'},
     'hypers_301x_1T2016': {'dbname': 'UQx_Hypers301x_1T2016', 'mongoname': 'course-v1:UQx+Hypers301x+1T2016', 'discussiontable': 'UQx-Hypers301x-1T2016-prod', 'csjson': 'UQx-Hypers301x-1T2016-course_structure-prod-analytics.json', 'icon': 'fa-plane', 'year': '2016', 'term': '1T'},
+    'hypers_301x_3T2016': {'dbname': 'UQx_Hypers301x_3T2016', 'mongoname': 'course-v1:UQx+Hypers301x+3T2016', 'discussiontable': 'UQx-Hypers301x-3T2016-prod', 'csjson': 'UQx-Hypers301x-3T2016-course_structure-prod-analytics.json', 'icon': 'fa-plane', 'year': '2016', 'term': '3T'},
 
     'ielts_x_3T2015': {'dbname':'UQx_IELTSx_3T2015', 'mongoname':'course-v1:UQx+IELTSx+3T2015', 'discussiontable': 'UQx-IELTSx-3T2015-prod', 'csjson': 'UQx-IELTSx-3T2015-course_structure-prod-analytics.json', 'icon':'fa-book', 'year':'2015', 'term': '3T'},
+    'ielts_x_3T2016': {'dbname':'UQx_IELTSx_3T2016', 'mongoname':'course-v1:UQx+IELTSx+3T2016', 'discussiontable': 'UQx-IELTSx-3T2016-prod', 'csjson': 'UQx-IELTSx-3T2016-course_structure-prod-analytics.json', 'icon':'fa-book', 'year':'2016', 'term': '3T'},
+
+    'learn_x_1T2017': {'dbname':'UQx_LEARNx_1T2017', 'mongoname':'course-v1:UQx+LEARNx+1T2017', 'discussiontable': 'UQx-LEARNx-1T2017-prod', 'csjson': 'UQx-LEARNx-1T2017-course_structure-prod-analytics.json', 'icon':'fa-graduation-cap', 'year':'2017', 'term': '1T'},
+
+	'lgdm_1x_1T2017': {'dbname':'UQx_LGDM1x_1T2017', 'mongoname':'course-v1:UQx+LGDM1x+1T2017', 'discussiontable': 'UQx-LGDM1x-1T2017-prod', 'csjson': 'UQx-LGDM1x-1T2017-course_structure-prod-analytics.json', 'icon':'fa-handshake-o', 'year':'2017', 'term': '1T'},
+	'lgdm_2x_1T2017': {'dbname':'UQx_LGDM2x_1T2017', 'mongoname':'course-v1:UQx+LGDM1x+2T2017', 'discussiontable': 'UQx-LGDM2x-1T2017-prod', 'csjson': 'UQx-LGDM2x-1T2017-course_structure-prod-analytics.json', 'icon':'fa-handshake-o', 'year':'2017', 'term': '1T'},
+	'lgdm_3x_1T2017': {'dbname':'UQx_LGDM3x_1T2017', 'mongoname':'course-v1:UQx+LGDM1x+3T2017', 'discussiontable': 'UQx-LGDM3x-1T2017-prod', 'csjson': 'UQx-LGDM3x-1T2017-course_structure-prod-analytics.json', 'icon':'fa-handshake-o', 'year':'2017', 'term': '1T'},
+	'lgdm_4x_1T2017': {'dbname':'UQx_LGDM4x_1T2017', 'mongoname':'course-v1:UQx+LGDM1x+4T2017', 'discussiontable': 'UQx-LGDM4x-1T2017-prod', 'csjson': 'UQx-LGDM4x-1T2017-course_structure-prod-analytics.json', 'icon':'fa-handshake-o', 'year':'2017', 'term': '1T'},
+
 
     'meta_101x_3T2015': {'dbname':'UQx_META101x_3T2015', 'mongoname':'course-v1:UQx+META101x+3T2015', 'discussiontable': 'UQx-META101x-3T2015-prod', 'csjson': 'UQx-META101x-1T2016-course_structure-prod-analytics.json', 'icon':'fa-coffee', 'year':'2015', 'term': '3T'},
     'meta_101x_1T2016': {'dbname': 'UQx_META101x_1T2016', 'mongoname': 'course-v1:UQx+META101x+1T2016', 'discussiontable': 'UQx-META101x-1T2016-prod', 'csjson': 'UQx-META101x-3T2015-course_structure-prod-analytics.json', 'icon': 'fa-coffee', 'year': '2016', 'term': '1T'},
@@ -39,18 +50,22 @@ EDX_DATABASES = {
     'think_101x_1T2014': {'dbname': 'UQx_Think101x_1T2014', 'mongoname': 'UQx/Think101x/1T2014', 'discussiontable': 'UQx-Think101x-1T2014-prod', 'csjson': 'UQx-Think101x-1T2014-course_structure-prod-analytics.json', 'icon': 'fa-heart', 'year': '2014', 'term': '1T'},
     'think_101x_2T2015': {'dbname': 'UQx_Think101x_2T2015', 'mongoname': 'course-v1:UQx+Think101x+2T2015', 'discussiontable': 'UQx-Think101x-2T2015-prod', 'csjson': 'UQx-Think101x-2T2015-course_structure-prod-analytics.json', 'icon':'fa-heart', 'year':'2015', 'term': '2T'},
     'think_101x_1T2016': {'dbname': 'UQx_Think101x_1T2016', 'mongoname': 'course-v1:UQx+Think101x+1T2016', 'discussiontable': 'UQx-Think101x-1T2016-prod', 'csjson': 'UQx-Think101x-1T2016-course_structure-prod-analytics.json', 'icon': 'fa-heart', 'year': '2016', 'term': '1T'},
+    'think_101x_3T2016': {'dbname': 'UQx_Think101x_3T2016', 'mongoname': 'course-v1:UQx+Think101x+3T2016', 'discussiontable': 'UQx-Think101x-3T2016-prod', 'csjson': 'UQx-Think101x-3T2016-course_structure-prod-analytics.json', 'icon': 'fa-heart', 'year': '2016', 'term': '3T'},
 
 
     'tropic_101x_1T2014': {'dbname': 'UQx_TROPIC101x_1T2014', 'mongoname': 'UQx/TROPIC101x/1T2014', 'discussiontable': 'UQx-TROPIC101x-1T2014-prod', 'csjson': 'UQx-Tropic101-3T2015-course_structure-prod-analytics.json', 'icon': 'fa-tree', 'year': '2014', 'term': '1T'},
     'tropic_101x_2T2015': {'dbname': 'UQx_Tropic101_2T2015', 'mongoname': 'UQx/TROPIC101x/2T2015', 'discussiontable': 'UQx-Tropic101-2T2015-prod', 'csjson': 'UQx-Tropic101-2T2015-course_structure-prod-analytics.json', 'icon': 'fa-tree', 'year': '2015', 'term': '2T'},
     'tropic_101x_3T2015': {'dbname': 'UQx_Tropic101_3T2015', 'mongoname': 'course-v1:UQx+Tropic101+3T2015', 'discussiontable': 'UQx-Tropic101-3T2015-prod', 'csjson': 'UQx-Tropic101-2T2016-course_structure-prod-analytics.json', 'icon': 'fa-tree', 'year': '2015', 'term': '3T'},
+    'tropic_101x_2T2016': {'dbname': 'UQx_Tropic101_2T2016', 'mongoname': 'course-v1:UQx+Tropic101+2T2016', 'discussiontable': 'UQx-Tropic101-2T2016-prod', 'csjson': 'UQx-Tropic101-2T2016-course_structure-prod-analytics.json', 'icon': 'fa-tree', 'year': '2016', 'term': '2T'},
 
     'world_101x_3T2014': {'dbname': 'UQx_World101x_3T2014', 'mongoname': 'UQx/World101x/3T2014', 'discussiontable': 'UQx-World101x-3T2014-prod', 'csjson': 'UQx-World101x-3T2014-course_structure-prod-analytics.json', 'icon': 'fa-map-marker', 'year': '2014', 'term': '3T'},
     'world_101x_2T2015': {'dbname': 'UQx_World101x_2T2015', 'mongoname': 'UQx/World101x/2T2015', 'discussiontable': 'UQx-World101x-2T2015-prod', 'csjson': 'UQx-World101x-2T2015-course_structure-prod-analytics.json', 'icon': 'fa-map-marker', 'year': '2015', 'term': '2T'},
+	'world_101x_2T2016': {'dbname': 'UQx_World101x_2T2016', 'mongoname': 'course-v1:UQx+World101x+2T2016', 'discussiontable': 'UQx-World101x-2T2016-prod', 'csjson': 'UQx-World101x-2T2016-course_structure-prod-analytics.json', 'icon': 'fa-map-marker', 'year': '2016', 'term': '2T'},
 
     'write_101x_3T2014': {'dbname': 'UQx_Write101x_3T2014', 'mongoname': 'UQx/Write101x/3T2014', 'discussiontable': 'UQx-Write101x-3T2014-prod', 'csjson': 'UQx-Write101x-3T2014-course_structure-prod-analytics.json', 'icon': 'fa-pencil', 'year': '2014', 'term': '3T'},
     'write_101x_2T2015': {'dbname': 'UQx_Write101x_2T2015', 'mongoname': 'UQx/Write101x/2T2015', 'discussiontable': 'UQx-Write101x-2T2015-prod', 'csjson': 'UQx-Write101x-2T2015-course_structure-prod-analytics.json', 'icon': 'fa-pencil', 'year': '2015', 'term': '2T'},
-    'write_101x_1T2016': {'dbname': 'UQx_Write101x_1T2016', 'mongoname': 'course-v1:UQx+Write101x+1T2016', 'discussiontable': '', 'csjson': 'UQx-Write101x-1T2016-course_structure-prod-analytics.json', 'icon': 'fa-pencil', 'year': '2016', 'term': '1T'},     
+    'write_101x_1T2016': {'dbname': 'UQx_Write101x_1T2016', 'mongoname': 'course-v1:UQx+Write101x+1T2016', 'discussiontable': 'UQx-Write101x-1T2016-prod', 'csjson': 'UQx-Write101x-1T2016-course_structure-prod-analytics.json', 'icon': 'fa-pencil', 'year': '2016', 'term': '1T'},     
+    'write_101x_2T2016': {'dbname': 'UQx_Write101x_2T2016', 'mongoname': 'course-v1:UQx+Write101x+2T2016', 'discussiontable': 'UQx-Write101x-2T2016-prod', 'csjson': 'UQx-Write101x-1T2016-course_structure-prod-analytics.json', 'icon': 'fa-pencil', 'year': '2016', 'term': '2T'},   
 }
 
 for DB in EDX_DATABASES:
